f84784734e2d27201b1893b280f9a70c58735c3a
Eng-Francisco-Hernandez <fjch87@gmail.com>
2023-06-16 17:46:44 -0600

    feat(frontend-development): added login and sign up pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Button, Card, Form } from "react-bootstrap";
 
 export default function Login() {
+  const handleSubmit = (event: any) => {
+    const form = event.currentTarget;
+    console.log(form);
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="auth-layout">
+      <Card className="auth-form">
+        <h2 className="title">Sign in</h2>
+        <Form onSubmit={handleSubmit}>
+          <Form.Label htmlFor="inputEmail">Email</Form.Label>
+          <Form.Control type="email" id="inputEmail" className="mb-3" />
+          <Form.Label htmlFor="inputPassword">Password</Form.Label>
+          <Form.Control type="password" id="inputPassword" className="mb-3" />
+          <div className="d-grid gap-2">
+            <Button type="submit">Login</Button>
+          </div>
+        </Form>
+        <p className="auth-already-done">
+          Not registered yet ? <a href="/signup">Sign up</a>
+        </p>
+      </Card>
+    </div>
+  );
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { Button, Card, Form } from "react-bootstrap";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="auth-layout">
+      <Card className="auth-form">
+        <h2 className="title">Sign Up</h2>
+        <Form>
+          <Form.Label htmlFor="inputEmail">Email</Form.Label>
+          <Form.Control type="email" id="inputEmail" className="mb-3" />
+
+          <Form.Label htmlFor="inputPassword">Password</Form.Label>
+          <Form.Control
+            type="password"
+            id="inputPassword"
+            aria-describedby="passwordHelpBlock"
+            className="mb-3"
+          />
+          <Form.Label htmlFor="inputPasswordConfirm">
+            Confirm password
+          </Form.Label>
+          <Form.Control type="password" id="inputPasswordConfirm" />
+          <Form.Text id="passwordHelpBlock" muted>
+            Your password must be 8-20 characters long, contain letters and
+            numbers, and must not contain spaces, special characters, or emoji.
+          </Form.Text>
+          <div>
+            <div className="d-grid gap-2">
+              <Button className="mt-3" type="submit">
+                Sign up
+              </Button>
+            </div>
+          </div>
+        </Form>
+        <p className="auth-already-done">
+          Already registered? <a href="/login">Sign in</a>
+        </p>
+      </Card>
+    </div>
+  );
 }
