891be6cb356fa000fbcd04b9fa9aa05354425505
Eng-Francisco-Hernandez <fjch87@gmail.com>
2023-09-03 19:53:02 -0600

    feat(frontend-development): fix router

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <ApolloProvider client={apolloClient}>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
     </HashRouter>
   </ApolloProvider>
