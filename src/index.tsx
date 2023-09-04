3b5548591fbae09331e0af1e6da6a6056284fc49
Eng-Francisco-Hernandez <fjch87@gmail.com>
2023-09-03 19:34:10 -0600

    feat(frontend-development): add fixes

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <ApolloProvider client={apolloClient}>
-        <App />
-      </ApolloProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
 );
