8b7e35ce52cf157a5c69819e17b842e04c8f2fb7
Eng-Francisco-Hernandez <fjch87@gmail.com>
2023-09-03 18:40:58 -0600

    feat(frontend-development): add healthcheck

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import { ROUTES } from "./navigation/routes";
 import "./App.scss";
 import { PrivateRoute } from "./components";
+import { useEffect } from "react";
 
 function App() {
   const authed =
     localStorage.getItem("accessToken") && localStorage.getItem("refreshToken");
+
+  // API Healthcheck
+  useEffect(() => {
+    async function healthCheck() {
+      const res = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL!}`);
+      const hc: any = await res.json();
+      console.log(hc.status);
+    }
+    healthCheck();
+  }, []);
+
   return (
     <Routes>
       <Route
