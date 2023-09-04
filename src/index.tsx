0d6689ff37dd399a26f2fa93e1902001f3550da1
Eng-Francisco-Hernandez <fjch87@gmail.com>
2023-09-03 19:49:54 -0600

    feat(frontend-development): add fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import { apolloClient } from "./lib";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <ApolloProvider client={apolloClient}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ApolloProvider>
 );
